c9773bc0d71125dad76528ac9f0412e1c7a55305
trung2515 <dinhtrung2515@gmail.com>
2025-10-10 14:44:35 +0700

    remove duplicate code in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,19 +17,6 @@ app.use(
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  }),
-);
-
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
 app.use('/api/auth', router);
 app.use('/api/employees', employeeRouter);
 app.use('/api/notification', notificationRouter);
